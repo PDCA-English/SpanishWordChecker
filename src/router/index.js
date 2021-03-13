e87a98c870a0e73d5b201704cd942074d7cf7ca0
Takuya <r28.6sa@gmail.com>
2021-03-13 20:59:56 +0900

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Main from '../views/Main.vue'
+import Quiz from '../views/Quiz.vue'
+import Finish from '../views/Finish.vue'
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Main',
+    component: Main
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/quiz',
+    name: 'Quiz',
+    component: Quiz
+  },
+  {
+    path: '/finish',
+    name: 'Finish',
+    component: Finish
+  },
 ]
 
 const router = new VueRouter({
---
+++ src/views/Main.vue
@@ -0,0 +1,212 @@
+<template>
+  <div class="main" id="app">
+    <div class="mainHeader">
+      <button>Start</button>
+      <ul>
+        <li>セクション数：--</li>
+        <li>フレーズ数：--</li>
+      </ul>
+      <ul>
+        <li>制限時間：<input type="checkbox"></li>
+        <li>ヒント：<input type="checkbox"></li>
+       </ul>
+    </div>
+    <div v-for="(chapter,index) in chapters" :key="index">
+      <h1>
+        <input type="checkbox" id="" value="">
+        Chapter{{index+1}}：{{chapter.name}}
+      </h1>
+      <ul v-for="(chapterContent,index) in chapter.chapterContents" :key="index">
+        <li>
+          <input type="checkbox" id="chapeterNum" value="" >
+          {{index+1}}. {{chapterContent.sectionName}}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chapters: [
+        {
+          id: 1,
+          name: "ミニ文法",
+          numOfSections: 7,
+          isSelected: false,
+          chapterContents: [
+            {
+              id: 1,
+              sectionName: "現在形",
+              isSelected: false,
+              sectionContents: [
+                {
+                  id: 1,
+                  jsentence: "私は（今）サンドイッチを食べています。",
+                  esentence: "Yo como un bocadillo ahora.",
+                  sectionNum: "101現在形"
+                },
+                {
+                  id: 2,
+                  jsentence: "私は（今）サンドイッチを食べています。（現在進行形）",
+                  esentence: "Yo estoy comiendo un bocadillo ahora.",
+                  sectionNum: "101現在形"
+                },
+                {
+                  id: 3,
+                  jsentence: "父はタバコを吸います。",
+                  esentence: "Mi padre fuma.",
+                  sectionNum: "101現在形"
+                },
+                {
+                  id: 4,
+                  jsentence: "アナは美しい。（アナは常に美しい）",
+                  esentence: "Ana es guapa.",
+                  sectionNum: "101現在形"
+                },
+                {
+                  id: 5,
+                  jsentence: "アナは今日きれいだ。（化粧や洋服などがいつもと違い、今日のアナはきれいだ）",
+                  esentence: "Ana está guapa hoy.",
+                  sectionNum: "101現在形"
+                },
+                {
+                  id: 6,
+                  jsentence: "私の家は福岡にある。",
+                  esentence: "Mi casa está en Fukuoka.",
+                  sectionNum: "101現在形"
+                },
+                {
+                  id: 7,
+                  jsentence: "パーティーは私の家で行われる。",
+                  esentence: "La fiesta es en mi casa.",
+                  sectionNum: "101現在形"
+                },
+                {
+                  id: 8,
+                  jsentence: "私は１時間以内に空港に着きます。",
+                  esentence: "Llego al aeropuerto en una hora.",
+                  sectionNum: "101現在形"
+                },
+                {
+                  id: 9,
+                  jsentence: "もうちょっとでつまずいて転ぶところだった。",
+                  esentence: "Casi tropiezo y me caigo.",
+                  sectionNum: "101現在形"
+                }
+              ]
+            },
+            {
+              id: 2,
+              sectionName: "過去形",
+              isSelected: false,
+              sectionContents: []
+            },
+            {
+              id: 3,
+              sectionName: "未来形",
+              isSelected: false,
+              sectionContents: []
+            },
+            {
+              id: 4,
+              sectionName: "進行形",
+              isSelected: false,
+              sectionContents: []
+            },
+            {
+              id: 5,
+              sectionName: "再帰動詞",
+              isSelected: false,
+              sectionContents: []
+            },
+            {
+              id: 6,
+              sectionName: "接続法",
+              isSelected: false,
+              sectionContents: []
+            },
+            {
+              id: 7,
+              sectionName: "目的語の代名詞",
+              isSelected: false,
+              sectionContents: []
+            }
+          ]
+        },
+        {
+          id: 2,
+          name: "文を作るための道具箱（構文編）",
+          numOfSections: 40,
+          isSelected: false,
+          chapterContents: [
+            {
+              id: 1,
+              sectionName: "〜がいた、あった（行われた）",
+              isSelected: false,
+              sectionContents: []
+            },
+            {
+              id: 2,
+              sectionName: "～な日だった、今日は～だった",
+              isSelected: false,
+              sectionContents: []
+            },
+            {
+              id: 3,
+              sectionName: "〜した、〜したことがある",
+              isSelected: false,
+              sectionContents: []
+            },
+            {
+              id: 4,
+              sectionName: "〜を始める、〜し始める",
+              isSelected: false,
+              sectionContents: []
+            },
+            {
+              id: 5,
+              sectionName: "〜を終える、〜し終える",
+              isSelected: false,
+              sectionContents: []
+            },
+            {
+              id: 6,
+              sectionName: "〜へ行く、〜しに行く",
+              isSelected: false,
+              sectionContents: []
+            },
+            {
+              id: 7,
+              sectionName: "〜が来る",
+              isSelected: false,
+              sectionContents: []
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
+</script>
+
+<style>
+ul {
+  list-style: none;
+}
+
+/* mainHeader */
+.mainHeader {
+  display: flex;
+}
+
+.mainHeader ul {
+  text-align: right;
+}
+
+
+
+
+</style>
